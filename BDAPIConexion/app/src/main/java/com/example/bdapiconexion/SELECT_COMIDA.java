0b68af6eb3438e0f42352668af28a0f32ff97330
EstebanVz <1930445@upt.edu.mx>
2023-08-13 04:03:29 -0600

    CRUD COMIDA

--- BDAPIConexion/app/src/main/java/com/example/bdapiconexion/SELECT_COMIDA.java
+++ BDAPIConexion/app/src/main/java/com/example/bdapiconexion/SELECT_COMIDA.java
@@ -2,13 +2,180 @@ package com.example.bdapiconexion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class select_comida extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SELECT_COMIDA extends AppCompatActivity {
+
+    Button Layout_CRUD;
+    LinearLayout Consulta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_comida);
+
+        Layout_CRUD = findViewById(R.id.Layout_CRUD);
+        Consulta = findViewById(R.id.Consulta);
+
+        TBDesayuno();
+        TBComida();
+        TBCena();
+        //Navigate
+        Layout_CRUD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(SELECT_COMIDA.this, CRUD_Comidas.class);
+                startActivity(intent);
+
+            }
+        });
     }
+
+    //https://apprestauranteupt.azurewebsites.net/api/ComidasApp?time=801
+    private void TBDesayuno() {
+        String url = "https://apprestauranteupt.azurewebsites.net/api/ComidasApp?time=801"; // URL que devuelve un arreglo de objetos JSON
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
+
+                            // Create a TextView for the header
+                            TextView headerTextView = new TextView(SELECT_COMIDA.this);
+                            headerTextView.setText("++ DESAYUNO ++");
+                            // Customize headerTextView appearance if needed
+                            Consulta.addView(headerTextView);
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                                TextView textView = new TextView(SELECT_COMIDA.this);
+                                textView.setText("" + jsonObject.getString("IdComida") +
+                                        " | " + jsonObject.getString("NombreComida") +
+                                        " | " + jsonObject.getString("Descripcion"));
+
+                                Consulta.addView(textView);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Error", error.getMessage());
+                    }
+                });
+
+        Volley.newRequestQueue(this).add(stringRequest);
+    }
+
+    //https://apprestauranteupt.azurewebsites.net/api/ComidasApp?time=1200
+    private void TBComida() {
+        String url = "https://apprestauranteupt.azurewebsites.net/api/ComidasApp?time=1201"; // URL que devuelve un arreglo de objetos JSON
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
+
+                            // Create a TextView for the header
+                            TextView headerTextView = new TextView(SELECT_COMIDA.this);
+                            headerTextView.setText("++ COMIDA ++");
+                            // Customize headerTextView appearance if needed
+                            Consulta.addView(headerTextView);
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                                TextView textView = new TextView(SELECT_COMIDA.this);
+                                textView.setText("" + jsonObject.getString("IdComida") +
+                                        " | " + jsonObject.getString("NombreComida") +
+                                        " | " + jsonObject.getString("Descripcion"));
+
+                                Consulta.addView(textView);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Error", error.getMessage());
+                    }
+                });
+
+        Volley.newRequestQueue(this).add(stringRequest);
+    }
+
+
+
+    //https://apprestauranteupt.azurewebsites.net/api/ComidasApp?time=1801
+    private void TBCena() {
+        String url = "https://apprestauranteupt.azurewebsites.net/api/ComidasApp?time=1801"; // URL que devuelve un arreglo de objetos JSON
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
+
+                            // Create a TextView for the header
+                            TextView headerTextView = new TextView(SELECT_COMIDA.this);
+                            headerTextView.setText("++ CENA ++ ");
+                            // Customize headerTextView appearance if needed
+                            Consulta.addView(headerTextView);
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                                TextView textView = new TextView(SELECT_COMIDA.this);
+                                textView.setText("" + jsonObject.getString("IdComida") +
+                                        " | " + jsonObject.getString("NombreComida") +
+                                        " | " + jsonObject.getString("Descripcion"));
+
+                                Consulta.addView(textView);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Error", error.getMessage());
+                    }
+                });
+
+        Volley.newRequestQueue(this).add(stringRequest);
+    }
+
 }
--- README.md
+++ README.md
@@ -5,35 +5,52 @@ Proyecto Final Unidad 03
 ### 1930445
 
 # INSTRUCCIONES
-
 - 3 Mesas y Meseros minimo 
-
-Para evaluar el tercer parcial se trabajará en un proyecto que se explicará en líneas más adelante, Teniendo como fecha límite el día especificado por la coordinación, y por ningún motivo se ofrecerá prorroga de entrega.
 
 El proyecto que tiene que realizar es el siguiente:
 
-Una aplicación que realice un punto de venta de una cafetería, dicha aplicación contendrá las siguientes características:
+Una aplicación que realice un punto de venta de una cafetería, 
+dicha aplicación contendrá las siguientes características:
 
-# Tres menús, dependiendo la hora se mostrará un menú, de las 8:00 a las 12:00 de manera automática se mostrará el menú de desayunos, de las 12:01 a las 18:00 se mostrará el menú de comidas y de las 18:01 a las 23:00 se mostrará el menú de las cenas.
+# Tres menús, dependiendo la hora se mostrará un menú:
 
-# De igual manera se mostrará una sección para que el usuario puedo pedir algún antojito que no estén en los menús.
+------COMPLETO--------de las 8:00 a las  12:00 de manera automática se mostrará el menú de desayunos, 
+------COMPLETO--------de las 12:01 a las 18:00 se mostrará el menú de comidas y 
+------COMPLETO--------de las 18:01 a las 23:00 se mostrará el menú de las cenas.
 
-# El usuario podrá seleccionar su comida y bebidas que desee y de manera automática le aparecerá el total de su cuenta la única información que necesita meter el usuario es el número de mesa que está ocupando.
 
-Se realizará una interfaz para el mesero en donde les aparecerá las ordenes que los clientes han realizaron de acuerdo a las mesas asignadas para cada cana. De igual manera les aparecerá el monto de cada mesa, una vez que el mesero autorice la orden, esta se visualizará en la interfaz de los cocineros.
+COMENSAL 
+--- VISTA   Contemplar el # de ma mesa
+---- Con la lista de comida y monto total de consumo 
+--COMPLETO--  donde puede colocar pedido de antojos extras
+--COMPLETO--  donde puede agregar mas refrescos 
+--COMPLETO--  donde puede agregar mas comida del menu 
+---- Cuando termina de comer debe de pedir cerrar la cuenta (mesero debe de confirmar el pago para cerrar la cuenta)
 
-Cuando los cocineros terminen de preparar la orden, le aparecerá una notificación al mesero para que la pueda entregarla, una vez que el cliente termina de consumir tiene que cerrar la cuanta    y la meza queda nuevamente libre.
+MESERO 
+---VISTA 
+-----COMPLETO (API-Agrupar por pedidos de mesa Consumir api Acciones)------  Mesero para hacer orden 
+-----COMPLETO----  donde puede agregar mas refrescos 
+-----COMPLETO----  donde puede agregar mas comida del menu 
+--COMPLETO (API-Agrupar por pedidos de mesa Consumir api Acciones)--- las mesas con sus respectivas ordenes
+--COMPLETO (API-Agrupar por pedidos de mesa Consumir api Acciones)--- cada mesa debera de tener el monto total a cobrar 
+--FALTA--- boton de pedir Orden cambiar status en todas las RER a 2 en preparacion
+--FALTA---/ Mesero debe de realizar cobro a mesa y confirmar el pago para cambiar status a 5 Pagado
+--FALTA---/ Mesero debe de limpiar mesa y order todo cambiar status a 6 Mesa Libre 
 
-La interfaz tiene que ser muy intuitiva recordando que no todas las personas saben manejar la tecnología.
+COCINERO 
+--COMPLETO (API-Agrupar por pedidos de mesa Consumir api Acciones)-- Vista de los pedidos que debe de realizar por mesa (Ver detalle de la orden)
+---- Comida y antojos que solicita cada mesa -- directo al cocinero Status 2
+---- Cuando esta listo el antojo -- pasa al mesero  status 3
+----//  Notificación cuando las ordenes estan listas para comida extra (antojitos)
 
-Cada mesero tiene que tener una cuanta y en cada venta realizada tiene que aparecer los datos del mesero.
+ADMIN
+---VISTA
+--COMPLETO-- Registrar Empleados con password
+---- Los empleados solo podrán cambiar la contraseña de su cuenta.
 
-En la interfaz de la cocina tiene que tener una sección donde el cocinero pueda seleccionar la orden que va a preparar.  También cada cocinero tiene que tener sui propia cuanta.
 
-En la aplicación tiene que haber una cuenta de administrador la cual es la encargada de registrar a los empleados y asignarle sus credenciales.
 
-# Los empleados solo podrán cambiar la contraseña de su cuenta.
 
-Se tiene que subir el repositorio de su proyecto y por lo menos tiene que subir una actualización al día de su proyecto, explicando en que consiste dicha actualización
-
-La aplicación solo se podrá evaluar si realiza el 100% de lo solicitado y por ningún motivo se podrá dar prorroga
+APi que cambie status por mesa 
+APi que cambie status por bebida, comida, comida extra 
