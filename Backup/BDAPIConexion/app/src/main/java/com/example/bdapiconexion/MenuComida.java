ff01dc6002ad9eb771dbdbeb07a93d6aae5e770c
EstebanVz <1930445@upt.edu.mx>
2023-08-13 04:44:19 -0600

    Restauracion Base APP
    Se trabajara en una restauracion dee la app antes de la actualizacion de paquetes

---
+++ Backup/BDAPIConexion/app/src/main/java/com/example/bdapiconexion/MenuComida.java
@@ -0,0 +1,23 @@
+package com.example.bdapiconexion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuComida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_comida);
+
+    }
+
+    public void listaComida(){
+
+
+
+    }
+
+
+}
