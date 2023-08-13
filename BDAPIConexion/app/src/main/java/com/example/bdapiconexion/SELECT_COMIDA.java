670675eaaafc1213e554f707b31d4f3f18a215c2
EstebanVz <angelesteban159@outlook.es>
2023-08-13 03:48:34 -0600

    CRUD Update (Basicos)

---
+++ BDAPIConexion/app/src/main/java/com/example/bdapiconexion/SELECT_COMIDA.java
@@ -0,0 +1,14 @@
+package com.example.bdapiconexion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class select_comida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_comida);
+    }
+}
